83db283b4aca9edfc025b445b3b915a6b5fb1edc
Mustafa <93279706+muhni97@users.noreply.github.com>
2022-11-26 00:00:12 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClNWItYAjXIrwy-rqqIoGSz2ND9iixBrM",
-  authDomain: "membersandgroups.firebaseapp.com",
-  projectId: "membersandgroups",
-  storageBucket: "membersandgroups.appspot.com",
-  messagingSenderId: "401734859321",
-  appId: "1:401734859321:web:b337e483189fc475f86be8"
+  apiKey: "**************************************",
+  authDomain: "**************************************",
+  projectId: "**************************************",
+  storageBucket: "**************************************",
+  messagingSenderId: "**************************************",
+  appId: "**************************************"
 };
 
 
@@ -15,8 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
-// const categoriesRef = collection(db, "categories")
 
 export {
   db
